5f5749980dd7d92f759ec587a7b255e5714bc2d6
akshathal <akshathal@LPT084-B1.quinnox.corp>
2015-04-07 15:59:39 +0530

    HTML Page Commit

--- config/router.js
+++ config/router.js
@@ -2,11 +2,20 @@
 *All the get and post request
 */
 var rsd = require('../app/controllers/rsd');
+var loadtest = require('../app/controllers/rsd')
 
 
 module.exports = function (app) {
 	
+	/*
+	 * Author:Thammaiah
+	 */
 	app.get('/rsdhome', rsd.showhomepage);
 	app.post('/rsdwelcomepage', rsd.showwelcomepage);
+	
+	/*
+	 * Author:Akshatha
+	 */
+	app.get('/loadtestcase',rsd.displayloadtest);
 	
 };
